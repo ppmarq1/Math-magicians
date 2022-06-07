23d13fee49b27bd12b8c96dfd6a812b6fc0613ed
Pedro Marques <ppmarq1@gmail.com>
2022-06-07 14:33:22 +0100

    Fix linters errors and delete unused file

--- src/components/Calcupage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Calcupage.css';
-
-export default function CalcPageText() {
-  return (
-    <p className="calcParagraph">Let&apos;s do some Maths!</p>
-  );
-}

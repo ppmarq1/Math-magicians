8397998946a03b9245162ab0818e1b66dc066ab0
Pedro Marques <ppmarq1@gmail.com>
2022-06-08 14:13:18 +0100

    Fix linter errors

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
 import Calculator from './components/Calculator';
-import Home from './components/Home';
+import Home from './components/home';
 import NavBar from './components/navbar';
 import Quotes from './components/quote';
 import calculate from './logic/calculate';
